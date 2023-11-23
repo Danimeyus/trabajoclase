4eec9f7d056ae21c76626d0ee108c57f4fb03463
Danimeyus <151823205+Danimeyus@users.noreply.github.com>
2023-11-23 09:32:56 +0100

    Add files via upload

---
+++ Argumentos/src/main/java/com/mycompany/argumentos/Argumentos.java
@@ -0,0 +1,28 @@
+package com.mycompany.argumentos;
+public class Argumentos {
+
+    public static void main(String[] args) {
+        String argumento1;
+        String argumento2;
+        
+        argumento1=args[0];
+      
+        
+        System.out.println("Argumento 1: "+argumento1);
+     
+        
+        switch(argumento1){
+            case "1":{
+                System.out.println("Idioma Español");
+                break;
+            }
+            case "2":{
+                System.out.println("Hi! I'm Muzzy");
+                break;
+            }
+            default :{
+                System.out.println("Argumento incorrecto");
+            }
+        }
+    }
+}
